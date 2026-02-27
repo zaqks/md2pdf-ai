4906e622e6ad2477d30c46510fe29a5d58843a86
zaqks <imzakbro@gmail.com>
2026-02-27 17:48:08 +0100

    theme

--- services/app/src/main.js
+++ services/app/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'github-markdown-css/github-markdown-light.css'
+import 'highlight.js/styles/github.css'
 
 import App from './App.vue'
 
