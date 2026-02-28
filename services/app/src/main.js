25d5d1701e43a62fc5552b2a2161021c757696ca
zaqks <imzakbro@gmail.com>
2026-02-28 17:40:27 +0100

    styling modules

--- services/app/src/main.js
+++ services/app/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import 'github-markdown-css/github-markdown-light.css'
 import 'highlight.js/styles/github-dark.css'
-import './markdown-custom.css'
+
+import './styles/style.css'
+import './styles/custom/macos-frame.css'
+
 
 import App from './App.vue'
 
