e6fc5a1a516e9f41b58c3d4de1a40459dfbfca69
zaqks <imzakbro@gmail.com>
2026-02-28 17:28:19 +0100

    code block styling

--- services/app/src/main.js
+++ services/app/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import 'github-markdown-css/github-markdown-light.css'
-import 'highlight.js/styles/github.css'
+import 'highlight.js/styles/github-dark.css'
+import './markdown-custom.css'
 
 import App from './App.vue'
 
